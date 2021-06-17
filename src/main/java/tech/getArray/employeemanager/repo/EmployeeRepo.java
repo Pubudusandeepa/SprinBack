8c9ba112636ce55cae03b07454c6b86150b1317a
Pubudusandeepa <ppsandeepa12@gmail.com>
2021-06-17 13:06:42 +0530

    add update

--- src/main/java/tech/getArray/employeemanager/repo/EmployeeRepo.java
+++ src/main/java/tech/getArray/employeemanager/repo/EmployeeRepo.java
@@ -3,5 +3,9 @@ package tech.getArray.employeemanager.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tech.getArray.employeemanager.model.Employee;
 
+import java.util.Optional;
+
 public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteAllById(Long id);
+    Optional<Employee> findEmployeeById(Long id);
 }
