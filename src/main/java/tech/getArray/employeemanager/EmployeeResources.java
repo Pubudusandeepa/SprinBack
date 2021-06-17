fd2f12d9e3cd74c70bf2b9ca0c732aa5f4b9a71b
Pubudusandeepa <ppsandeepa12@gmail.com>
2021-06-17 10:11:32 +0530

    add employe contoller

---
+++ src/main/java/tech/getArray/employeemanager/EmployeeResources.java
@@ -0,0 +1,10 @@
+package tech.getArray.employeemanager;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeResources {
+}
