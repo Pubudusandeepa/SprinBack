fe0d4d0d190fab6df61601fc4ad257cf35516b44
Pubudusandeepa <ppsandeepa12@gmail.com>
2021-06-17 09:13:25 +0530

    add findall employees method

--- src/main/java/tech/getArray/employeemanager/service/EmployeeService.java
+++ src/main/java/tech/getArray/employeemanager/service/EmployeeService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import tech.getArray.employeemanager.model.Employee;
 import tech.getArray.employeemanager.repo.EmployeeRepo;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -19,6 +20,10 @@ public class EmployeeService {
     public Employee addEmployee(Employee employee) {
         employee.setEmployeeCode(UUID.randomUUID().toString());
         return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployees(){
+        return employeeRepo.findAll();
     }
 
 
