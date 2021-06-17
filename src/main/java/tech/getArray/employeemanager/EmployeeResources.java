88fdce7a21d11939d2e501bc0bf4dedc528e04d9
Pubudusandeepa <ppsandeepa12@gmail.com>
2021-06-17 10:38:12 +0530

    Add get Employee by id contoller method

--- src/main/java/tech/getArray/employeemanager/EmployeeResources.java
+++ src/main/java/tech/getArray/employeemanager/EmployeeResources.java
@@ -1,10 +1,42 @@
 package tech.getArray.employeemanager;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tech.getArray.employeemanager.model.Employee;
+import tech.getArray.employeemanager.service.EmployeeService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/employee")
 public class EmployeeResources {
+    private final EmployeeService employeeService;
+
+
+    public EmployeeResources(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        List<Employee> employees = employeeService.findAllEmployees();
+        return new ResponseEntity<List<Employee>>(employees, HttpStatus.OK);
+
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Employee> getAllEmployees(@PathVariable("id") Long id) {
+        Employee employee = employeeService.findEmployeeById(id);
+        return new ResponseEntity<>(employee, HttpStatus.OK);
+
+    }
+
+
+
+
 }
